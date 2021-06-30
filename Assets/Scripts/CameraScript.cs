45e9ce9c327979732d3b8f65e23be9e52a84d576
Pamdo <uzman.mustafa43@gmail.com>
2021-06-30 23:40:52 +0300

    rep

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -14,22 +14,35 @@ public class CameraScript : MonoBehaviour
 
     public CharacterMovement _characterMovement;
 
+    [SerializeField] private Transform _initialCam;
+
+    private void Start()
+    {
+        
+    }
     private void LateUpdate()
     {
-        if (_isFollowCam)
+        if(_characterMovement._isFollowCam && GameManager.Instance.isLevelStarted==false)
+        {
+            InitialCam();
+        }
+        if (_characterMovement. _isFollowCam&& GameManager.Instance.isLevelStarted)
         {
             FollowCam();
         }
-        if (_isLastPose)
+        if (_characterMovement. _isLastPose&& GameManager.Instance.isLevelStarted)
         {
             LastPose();
         }
         
     }  
+
+
     void FollowCam()
     {
         _desiredPosition = _player.transform.position + _cameraOffset;
         transform.position = Vector3.Lerp(transform.position, _desiredPosition, _smoothed);
+        transform.LookAt(_player.transform);
         Debug.Log("followcam");
     }
     void LastPose()
@@ -37,4 +50,10 @@ public class CameraScript : MonoBehaviour
         transform.LookAt(_player.transform, Vector3.left);
         Debug.Log("lastpose");
     } 
+    void InitialCam()
+    {
+        Debug.Log("initial cam00");
+        transform.position = _initialCam.position;
+        transform.LookAt(_player.transform);
+    }
 }
